e1300cfa4363c0a4bf18ced7e3731a18ddcd2f7d
Ivano PETTY <96083465+Ivano-P@users.noreply.github.com>
2023-12-07 16:11:55 +0100

    bug correction in controller, added dev tools to project

--- portfolio-front/src/main/java/com/ivanopetty/portfoliofront/controller/NavigationController.java
+++ portfolio-front/src/main/java/com/ivanopetty/portfoliofront/controller/NavigationController.java
@@ -1,13 +1,15 @@
 package com.ivanopetty.portfoliofront.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.service.annotation.GetExchange;
 
 @Controller
 public class NavigationController {
 
-    @GetExchange("/")
+    @GetMapping("/")
     public String goToPortfolioIndex(){
         return "index";
     }
+
 }
